9675a05bd98fe1da2890b6a1b76fd8adb3367b9c
Craig N. Caroon <craig.caroon@cengage.com>
2025-09-23 15:43:54 -0400

    --- 2024 // day12 --- * Replaced Makefile with Invoke tasks.py * Modelled a Plot instead of just keeping track of R,C locations * ...working on part2 ...

--- 2024/day12/garden.py
+++ 2024/day12/garden.py
@@ -3,28 +3,83 @@
 from shared.direction import Direction
 from shared.location import Location
 
+class Plot:
+
+    BOT_LEFT = (0,0)
+    BOT_RGHT = (1,0)
+    TOP_LEFT = (0,1)
+    TOP_RGHT = (1,1)
+
+    SIDE_TOP = (TOP_LEFT, TOP_RGHT)
+    SIDE_LEFT = (TOP_LEFT, BOT_LEFT)
+    SIDE_BOTTOM = (BOT_LEFT, BOT_RGHT)
+    SIDE_RIGHT = (TOP_RGHT, BOT_RGHT)
+
+    VERTEX_NAMES = {
+        "top-left": TOP_LEFT,
+        "top-right": TOP_RGHT,
+        "bottom-left": BOT_LEFT,
+        "bottom-right": BOT_RGHT
+    }
+
+    def __init__(self, location):
+        self.__loc = location
+
+        # Plots are Square. They have 4 corners/vertices
+        # self.__vertices = (
+        #     self.TOP_LEFT, self.TOP_RGHT,
+        #     self.BOT_LEFT, self.BOT_RGHT
+        # )
+
+
+    @property
+    def location(self):
+        return self.__loc
+
+
+    def __str__(self):
+        return str(self.__loc)
+
+
+    def __eq__(self, other):
+        return self.__loc == other.__loc
+
+
+    def __hash__(self):
+        return hash((self.__loc))
+
+
+    def vertex_uid(self, name:str):
+        """ Unique ID for one of the four vertexes by name """
+        vtx = self.VERTEX_NAMES.get(name)
+        if vtx is None:
+            raise ValueError(f"Invalid Vertex Name: '{name}'")
+
+        return f"{self.__loc.row},{self.__loc.col}:{vtx[0]},{vtx[1]}"
+
+
 class Region:
     def __init__(self, plant_type:str):
         self.__plant_type = plant_type
         # The Locations of each Plot in the Region
-        self.__plot_locations = set()
+        self.__plots = set()
 
 
     def __str__(self):
-        return f"{self.__plant_type} : {self.__plot_locations}"
+        return f"{self.__plant_type} : {self.__plots}"
 
 
     def __repr__(self):
         return str(self)
 
 
-    def __contains__(self, item:Location):
-        return item in self.__plot_locations
+    def __contains__(self, item:Plot):
+        return item in self.__plots
 
 
     @property
     def area(self):
-        return len(self.__plot_locations)
+        return len(self.__plots)
 
 
     @property
@@ -35,24 +90,60 @@ class Region:
 
         This Region has 8 sides:
 
-            [(1,2), (2,2), (2,3), (3,3)]
+              1
+            +---+
+            | C | 2,3
+          8 |   +---+
+            | C   C |
+            +---+   | 4
+            6,7 | C |
+                +---+
+                  5
 
-            +-+
-            |C|    ->
-            + +-+
-            |C C|  ->
-            +-+ +
-              |C|  ->
-              +-+
+        This Region has 4 sides:
 
+                1
             +-+-+-+-+
-            |R R R R|
-            |R R R R|
+            |R R R R| 2
+          4 |R R R R|
             +-+-+-+-+
+                3
+
+              0 1 2 3
+            0 A A A A
+            1 B B C D
+            2 B B C C
+            3 E E E C
+
+
+            1 => 4
+            2 => 4 - (2 + 2) = 0 => 4
+            3 => 4 - (2)     = 2 => 6
+            4 => 4 - (2)     = 2 => 8
         """
-        side_count = 1
+        # Number of sides == Number of vertices
+        # Minimum number of sides is 4, i.e. it has to at least be a rectangle
+        # Assume a Rectangle
+        side_count = 4
+
+        counted_vtxs = {}
+
+        for plot in self.__plots[1:]:
+            for direction in Direction.enumerate(("N","E","S","W")):
+                non_shared_sides = 4
+                # N => shares TOP
+                if direction.code == "N":
+                    shared_side = Plot.SIDE_TOP
+                    # vtx1 =
+
+                # S => shares BOTTOM
+                # E => shares RIGHT
+                # W => shares LEFT
 
 
+                neighbor = plot.location + direction
+                if neighbor in self.__plots:
+                    pass
 
         return side_count
 
@@ -70,48 +161,59 @@ class Region:
               |C|  -> 3
               +-+
         """
-        perimiter = 0
+        perimeter = 0
 
-        for plot_loc in self.__plot_locations:
+        for plot in self.__plots:
             # Assume 4 side are open
             plot_perimeter = 4
             # Look for sides that touch other plots
             # and reduce the plot_perimeter count
             for direction in Direction.enumerate(("N","E","S","W")):
-                neighbor = plot_loc + direction
-                if neighbor in self.__plot_locations:
+                neighbor = self.get_plot(plot.location + direction)
+                if neighbor in self.__plots:
                     plot_perimeter -= 1
 
-            perimiter += plot_perimeter
+            perimeter += plot_perimeter
 
-        return perimiter
+        return perimeter
 
 
     @property
     def is_empty(self):
-        return len(self.__plot_locations) == 0
+        return len(self.__plots) == 0
 
 
-    def sorted_plots(self):
-        plots = list(self.__plot_locations)
-        plots.sort()
-        return plots
+    def get_plot(self, location:Location):
+        found_plot = None
+        for plot in self.__plots:
+            if plot.location == location:
+                found_plot = plot
+                break
+
+        return found_plot
 
 
     def add_plot(self, location:Location):
-        self.__plot_locations.add(location)
+        self.__plots.add(Plot(location))
 
 
     def remove_plot(self, location:Location):
-        if location in self.__plot_locations:
-            self.__plot_locations.remove(location)
+        plot = self.get_plot(location)
+        if plot in self.__plots:
+            self.__plots.remove(location)
 
 
-    def touches(self, location:Location):
+    def touches(self, other):
         is_touching = False
 
-        for plot_loc in self.__plot_locations:
-            if location.adjacent_to(plot_loc):
+        other_loc = None
+        if isinstance(other, Location):
+            other_loc = other
+        elif isinstance(other, Plot):
+            other_loc = other.location
+
+        for plot in self.__plots:
+            if plot.location.adjacent_to(other_loc):
                 is_touching = True
                 break
 
@@ -132,7 +234,9 @@ class Garden:
 
     MARKER = "."
 
-    def __init__(self, filename:str):
+    def __init__(self, filename:str, **kwargs):
+        self.__debug = kwargs.get("debug", False)
+
         self.__filename = filename
 
         self.__rows = None
@@ -156,6 +260,8 @@ class Garden:
 
 
     def __load_garden_map(self):
+        self.__print_dbg("Loading Garden Map")
+
         self.__garden_map = []
         with open(self.__filename, "r") as fptr:
             while line := fptr.readline():
@@ -174,6 +280,7 @@ class Garden:
 
 
     def __make_regions(self, plant_type:str, plot_locs:list[Location]):
+        self.__print_dbg("Making Regions")
         curr_plot_count = len(plot_locs)
 
         # Start a region
@@ -208,6 +315,8 @@ class Garden:
 
         Hands each entry in that list off to the Region Maker(tm)
         """
+        self.__print_dbg("Identifying Regions")
+
         plot_map = {}
         for ridx in range(self.__rows):
             for cidx in range(self.__cols):
@@ -226,11 +335,14 @@ class Garden:
 
 
     def calculate_fencing(self, with_discount=False):
+        self.__print_dbg("Calculating Fencing")
         total_cost = 0
 
         for region in self.__regions:
             if with_discount:
-                region_cost = region.area * region.sides
+                # region_cost = region.area * region.sides
+                region_cost = 0
+                print(region)
             else:
                 region_cost = region.area * region.perimeter
             # print(f"{region} = {region.area}x{region.perimeter} == ${region_cost}")
@@ -238,6 +350,11 @@ class Garden:
 
         return total_cost
 
+
+    def __print_dbg(self, msg):
+        """ Print a message if `debug` is ON """
+        if self.__debug:
+            print(f"...{msg}...")
 
 
 
---
+++ 2024/day12/tasks.py
@@ -0,0 +1,46 @@
+from invoke import task
+
+from garden import Garden
+
+@task
+def part1(ctx, input_file="garden-plot.map"):
+    """Day 12 // Part 01"""
+
+    print(part1.__doc__)
+
+    garden = Garden(input_file)
+    fencing_cost = garden.calculate_fencing()
+
+    print(f"""
+-> Input File: {input_file}
+-> Plant Types: {garden.plant_types}
+-> Regions: {len(garden.regions)}
+-> Fencing Cost: ${fencing_cost}
+""")
+
+
+@task
+def part2(ctx, input_file="garden-plot.map", debug=False):
+    """Day 12 // Part 02"""
+
+    print(part2.__doc__)
+
+    garden = Garden(input_file, debug=debug)
+    # print(garden)
+    fencing_cost = garden.calculate_fencing(with_discount=True)
+
+    if debug:
+        import pprint
+        pprint.pprint(garden.regions)
+        region = garden.regions[2]
+        print("-------")
+        pprint.pprint(region.sorted_plots())
+        print(region.sides)
+
+
+    print(f"""
+-> Input File: {input_file}
+-> Plant Types: {garden.plant_types}
+-> Regions: {len(garden.regions)}
+-> Fencing Cost: ${fencing_cost}
+""")
