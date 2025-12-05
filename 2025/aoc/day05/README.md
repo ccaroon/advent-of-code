688ec1ecf0411e41624a6f14509fdab76ee37978
Craig N. Caroon <craig.caroon@cengage.com>
2025-12-05 12:10:21 -0500

    * Day 5 - Part 2 - In Progress...

--- 2025/aoc/day05/README.md
+++ 2025/aoc/day05/README.md
@@ -3,3 +3,34 @@ https://adventofcode.com/2025/day/5
 
 * Part 1: `inv run day05 1` => `789`
 * Part 2: `inv run day05 2` => `?????`
+
+
+
+
+## Scratch
+
+### Tries
+1. `269068590648661` -- too low
+2. `302272241966923` -- too low
+3. `308921066002250` -- too low
+
+### Unfeasible solutions
+```python
+    # expand the range & add to unique list
+    # entire number list is in memory :(
+    # ---NO: runs out of memory (killed)---
+    for iid in range(irng[0], irng[1]+1):
+        fresh_iids[iid] = None
+```
+
+```python
+    # Look at each iid in the range w/o keeping the entire
+    # range in memory
+    # ---NO: just too many numbers (billions) in each range (killed)---
+    start = irng[0]
+    end = irng[1]
+    iid = start
+    while iid >= start and iid <= end:
+        fresh_iids[iid] = None
+        iid += 1
+```
--- 2025/aoc/day05/puzzle.py
+++ 2025/aoc/day05/puzzle.py
@@ -48,5 +48,72 @@ class Cafeteria(Puzzle):
 
         return fresh_count
 
+    def __find_overlap(self, irng, other_ranges):
+        # 12-18 (12,13,14,15,16,17,18) | 10-14 (10,11,12,13,14)
+        # -> check start and end for overlap in any previous ranges
+        # overlap: my start to other end
+        # overlap: 12 to 14
+        # overlap: 14-12+1 = 3
+        # 7(range) - 3(overlap) => 4
+        # ---
+        # 12-18 (12,13,14,15,16,17,18) | 16-20 (16,17,18,19,20)
+        overlap_count = 0
+        start = irng[0]
+        end = irng[1]
+
+        for other_rng in other_ranges:
+            if start >= other_rng[0] and start <= other_rng[1]:
+                overlap_count += (other_rng[1] - start) + 1
+                self._debug(f"{irng} overlaps {other_rng} @ start[{start}]: {overlap_count}")
+            elif end >= other_rng[0] and end <= other_rng[1]:
+                self._debug(f"{irng} overlaps {other_rng} @ end[{end}]: {overlap_count}")
+                overlap_count += (end - other_rng[0]) + 1
+
+        return overlap_count
+
     def _part2(self):
-        pass
+        processed_ranges = []
+        fresh_iids = 0
+        for irng in self.__iid_ranges:
+            count = (irng[1] - irng[0]) + 1
+            overlap_count = self.__find_overlap(irng, processed_ranges)
+            count -= overlap_count
+
+            fresh_iids += count
+            processed_ranges.append(irng)
+            # ------------------
+            # 1655071119701-2036139951240
+            # 282420790922884-290825089106666
+            # find where ranges overlay
+            #
+            # 3-5   => 5-3+1   => 3
+            # is 3-5 covered by any previous range? NO +3
+
+            # 10-14 => 14-10+1 => 5
+            # is 10-15 covered by any previous range? NO +5
+
+            # 16-20 => 20-16+1 => 5
+            # is 16-20 covered by any previous range? NO +5
+
+            # 12-18 => 18-12+1 => 7
+            # is 12-18 covered by any previous range? YES
+
+            # 12-18 (12,13,14,15,16,17,18) | 10-14 (10,11,12,13,14)
+            # -> check start and end for overlap in any previous ranges
+            # overlap: my start to other end
+            # overlap: 12 to 14
+            # overlap: 14-12+1 = 3
+            # 7(range) - 3(overlap) => 4
+
+            # 12-18 (12,13,14,15,16,17,18) | 16-20 (16,17,18,19,20)
+            # overlap: my end to other end
+            # overlap: 18 to 20
+            # overlap: 20-18+1 = 3
+            # 4(overlap1) - 3(overlap) => 1
+            # +1
+
+
+        return fresh_iids
+
+
+#
