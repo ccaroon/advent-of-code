f5345b75cc67171459286575b92a8982e75f2c78
Craig N. Caroon <craig.caroon@cengage.com>
2025-12-09 17:43:58 -0500

    * Day 9 - Part 2 - In Progress...

--- 2025/aoc/day09/puzzle.py
+++ 2025/aoc/day09/puzzle.py
@@ -17,9 +17,9 @@ class MovieTheater(Puzzle):
         (row, col) = line.split(",", 2)
         self.__data.append(Location(int(row), int(col)))
 
-    def __compute_area(self, p1, p2):
-        rows = abs(p1.row - p2.row) + 1
-        cols = abs(p1.col - p2.col) + 1
+    def __compute_area(self, l1, l2):
+        rows = abs(l1.row - l2.row) + 1
+        cols = abs(l1.col - l2.col) + 1
 
         return rows * cols
 
@@ -33,5 +33,43 @@ class MovieTheater(Puzzle):
 
         return max_area
 
+
+    # def __find_
+
     def _part2(self):
-        pass
+        # map out the corners of the Loop
+        # for each location
+        # find all locs on same row
+        #   - before it that are RED
+        #   - after it that are RED
+        # find all locs on same col
+        #   - above it that are RED
+        #   - below it that are RED
+        # keep track of each end point in a set
+        #   - can a run of red.green+.red be contained in another run?
+        bbox_corners = set()
+        for loc in self.__data:
+            self._debug(f"=> {loc}...")
+            same_row = list(filter(lambda l: l.row == loc.row, self.__data))
+            same_col = list(filter(lambda l: l.col == loc.col, self.__data))
+
+            bbox_corners.update(same_row)
+            bbox_corners.update(same_col)
+
+            # if len(same_row) != 2:
+            #     self._debug(f"  -> Row: {same_row}")
+
+            # if len(same_col) != 2:
+            #     self._debug(f"  -> Col: {same_col}")
+
+        # print(bbox_corners)
+
+
+
+
+
+
+
+
+
+#
